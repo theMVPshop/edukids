979176d6d73f4ea8fca60d6c6a5ee9733a98eab3
tonezone108 <tonezone108@gmail.com>
2021-09-21 20:00:39 -0500

    Added About component and router

--- edukids/src/Router.js
+++ edukids/src/Router.js
@@ -3,7 +3,7 @@ import {Switch, Route} from "react-router";
 import TestComponent1 from "./components/TestComponent1"
 import TestComponent2 from "./components/TestComponent2";
 import TestHome from "./components/TestHome";
-
+import About from "./components/About";
 
 const Router = () => {
     return (
@@ -11,6 +11,7 @@ const Router = () => {
         <Route exact path="/" component={TestHome} />
         <Route exact path="/Test1" component={TestComponent1} />
         <Route exact path="/Test2" component={TestComponent2} />
+        <Route exact path="/About" component={About} />
         </Switch>
     )
 }
---
+++ edukids/src/components/About.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+// import { Button } from "@material-ui/core";
+
+class ContactUs extends Component {
+
+
+    render() {
+        return (
+            <div>
+               <p>Hello this is test About</p>
+            </div>
+            
+        )
+    }
+}
+
+export default ContactUs
