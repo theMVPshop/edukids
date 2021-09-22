00d57bf46ed3136f00a4a5ba4cabf761c837c5f3
BenABaron <benabaron@gmail.com>
2021-09-21 19:51:46 -0500

    Added career component and route

--- edukids/src/Router.js
+++ edukids/src/Router.js
@@ -3,6 +3,7 @@ import {Switch, Route} from "react-router";
 import TestComponent1 from "./components/TestComponent1"
 import TestComponent2 from "./components/TestComponent2";
 import TestHome from "./components/TestHome";
+import Career from './components/Career';
 
 
 const Router = () => {
@@ -11,6 +12,7 @@ const Router = () => {
         <Route exact path="/" component={TestHome} />
         <Route exact path="/Test1" component={TestComponent1} />
         <Route exact path="/Test2" component={TestComponent2} />
+        <Route exact path="/Career" component={Career} />
         </Switch>
     )
 }
