3af7e09d48f798e00e284823d5e5abd2d7dd5ac5
Lance Hammond Jr <lance.hammondjr@gmail.com>
2021-09-21 20:05:30 -0500

    Lance's Route Update

--- edukids/src/Router.js
+++ edukids/src/Router.js
@@ -4,6 +4,7 @@ import TestComponent1 from "./components/TestComponent1"
 import TestComponent2 from "./components/TestComponent2";
 import TestHome from "./components/TestHome";
 import Career from './components/Career';
+import Event from "./components/Events";
 
 
 const Router = () => {
@@ -13,6 +14,7 @@ const Router = () => {
         <Route exact path="/Test1" component={TestComponent1} />
         <Route exact path="/Test2" component={TestComponent2} />
         <Route exact path="/Career" component={Career} />
+        <Route exact path="/Event" component={Event} />
         </Switch>
     )
 }
