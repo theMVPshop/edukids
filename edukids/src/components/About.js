7d0580444997a7ecb88770f945b2c4d6fa492426
tonezone108 <tonezone108@gmail.com>
2021-09-22 19:59:56 -0500

    Added barebones About Component

--- edukids/src/components/About.js
+++ edukids/src/components/About.js
@@ -7,10 +7,48 @@ class About extends Component {
 
     render() {
         return (
+            
             <Container>
-            <Col>1 of 3</Col>
-            <Col>2 of 3</Col>
-            <Col>3 of 3</Col>
+                <Row className="justify-content-md-center">
+    <Col md="auto">We Are Kindergarten And Childhood Is Our Passion</Col>
+  </Row>
+  <Row className="justify-content-md-center">
+  <Col md="auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Tellus rutrum tellus pellentesque eu tincidunt tortor. Cras fermentum odio eu feugiat. Pellentesque massa placerat duis ultricies. 
+    Ipsum dolor sit amet consectetur adipiscing elit. Nulla porttitor massa id neque aliquam. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. </Col>
+      </Row>
+      <Container>
+          
+      </Container>
+      <Row>
+          <Col>Put Image here. </Col>
+          <Col md="auto">Put a title and then put some lorem ipsum and some bullets here</Col>
+    </Row>
+    <Row>
+          <Col md="auto">Put a title and then put some lorem ipsum and some bullets here</Col>
+          <Col>Put Image here. </Col>
+    </Row>
+
+    <Row>
+        <Col>
+        <Row className="justify-content-md-center">Class Sessions</Row>
+        <Row className="justify-content-md-center">Sunday - Monday</Row>
+        <Row className="justify-content-md-center">8:00 am - 12:30 pm</Row>
+        </Col>
+        <Col>
+        <Row className="justify-content-md-center">Location Address</Row>
+        <Row className="justify-content-md-center">145 While Kateway</Row>
+        <Row className="justify-content-md-center">CA 25615, San Francisco</Row>
+        </Col>
+        <Col>
+        <Row className="justify-content-md-center">Our Office Hour</Row>
+        <Row className="justify-content-md-center">Sunady - Monday</Row>
+        <Row className="justify-content-md-center">7:00 am - 5:30pm</Row></Col>
+        <Col>
+        <Row className="justify-content-md-center">Phone & Email</Row>
+        <Row className="justify-content-md-center">(+22)-658-258 / +259-55-9825</Row>
+        </Col>
+    </Row>
             </Container>
 
             
