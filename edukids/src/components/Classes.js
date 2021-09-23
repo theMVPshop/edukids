592884ad7a1f1b51c32d724493e17e0b01363310
eyokoo <70350730+eyokoo@users.noreply.github.com>
2021-09-22 20:03:52 -0500

    added cards

--- edukids/src/components/Classes.js
+++ edukids/src/components/Classes.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+
+
+
 
 function Classes(props) {
   return (
     <div>
+      
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src="https://images.unsplash.com/photo-1505672678657-cc7037095e60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" />
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
@@ -14,7 +18,32 @@ function Classes(props) {
     </Card.Text>
         </Card.Body>
       </Card>
+
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://images.unsplash.com/photo-1505672678657-cc7037095e60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+    </Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://images.unsplash.com/photo-1505672678657-cc7037095e60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+    </Card.Text>
+        </Card.Body>
+      </Card>
+
     </div>
+
+
   )
 }
 
