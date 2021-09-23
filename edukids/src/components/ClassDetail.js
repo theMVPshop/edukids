c0e446b734843c36b9073621b813968f4052788f
BrandonPerry-co <brandonperrymba@gmail.com>
2021-09-22 18:07:47 -0600

    Added class detail component

---
+++ edukids/src/components/ClassDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ClassDetail() {
+  return(
+    <div>
+      <h3>Class Detail</h3>
+      <p>Class Description</p>
+    </div>
+  )
+} 
+
+export default ClassDetail;
