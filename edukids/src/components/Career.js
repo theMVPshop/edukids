2714131b818fd76e95808ee682832002c824fff4
BenABaron <benabaron@gmail.com>
2021-09-22 19:54:08 -0500

    Built out barebones career page

--- edukids/src/components/Career.js
+++ edukids/src/components/Career.js
@@ -1,20 +1,104 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Card, Form } from 'react-bootstrap';
 
 function Career(props) {
+
   return(
     <Container>
       <Row>
-        <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
+        <Card>
+          <Card.Body>
+            <Card.Title>Let's join with us!</Card.Title>
+            <Card.Text>lorem ipsum</Card.Text>
+          </Card.Body>
+        </Card>
       </Row>
       <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
+        <Col>
+          <Row>
+            <Card>
+              <Card.Title>Teacher</Card.Title>
+              <Card.Text>teacher job description</Card.Text>
+            </Card>
+          </Row>
+          <Row>
+            <Card>
+              <Card.Title>Assistant</Card.Title>
+              <Card.Text>assistant job description</Card.Text>
+            </Card>
+          </Row>
+        </Col>
+        <Col>
+          <Row>
+            <Card>
+              <Card.Title>Manager</Card.Title>
+              <Card.Text>manager job description</Card.Text>
+            </Card>
+          </Row>
+          <Row>
+            <Card>
+              <Card.Title>Accountant</Card.Title>
+              <Card.Text>accountant job description</Card.Text>
+            </Card>
+          </Row>
+        </Col>
+        <Col>
+          <Form>
+            <Form.Group>
+              <Form.Label>Join Our Team</Form.Label>
+              <Form.Control type="text" placeholder="Your Name" />
+              <Form.Control type="email" placeholder="Your Email" />
+              <Form.Control type="text" placeholder="Your Position" />
+              <Form.Control type="text" placeholder="Enter Your Message" />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Our Benefits</Card.Title>
+              <Card.Text>Our school has been present for over 15 years to provide best quality education for kids.</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card body>Lorem ipsum</Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Img></Card.Img>
+              <Card.Title>Award Winning</Card.Title>
+              <Card.Text>Lorem ipsum</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Img></Card.Img>
+              <Card.Title>Affordable Fees</Card.Title>
+              <Card.Text>Lorem ipsum</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Img></Card.Img>
+              <Card.Title>Best Environment</Card.Title>
+              <Card.Text>Lorem ipsum</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </Container>
   )
+
 } 
 
 export default Career;
