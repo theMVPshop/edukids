031fc59fb7a30a2757f2846d084fc5eabc47273a
Lance Hammond Jr <lance.hammondjr@gmail.com>
2021-09-23 19:57:05 -0500

    Lance's Update

--- edukids/src/components/Events.js
+++ edukids/src/components/Events.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
 const Event = () => {
 
     return (
         <div>
-            
+           <Header></Header> 
 
         </div>
     );
--- edukids/src/components/Header.js
+++ edukids/src/components/Header.js
@@ -1,12 +1,50 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
 
 const Header = () => {
 
     return (
-        <div>
-            
+        <div className="App">
+        <Navbar bg="dark" variant="dark"
+            sticky="top" expand="lg">
+        <Navbar.Brand href="home" className="justify-content-start" >Logo</Navbar.Brand>
 
-        </div>
+            <Container>
+                <Container>
+                <Nav className="justify-content-center">
+                    
+                        <Nav.Link href="home">Home</Nav.Link>
+                        <Nav.Link href="about">About</Nav.Link>
+                        <NavDropdown title="Class">
+                            <NavDropdown.Item href="class/stuff">Stuff</NavDropdown.Item>
+                            <NavDropdown.Item href="class/stuff">Stuff</NavDropdown.Item>
+                            <NavDropdown.Item href="class/stuff">Stuff</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Event">
+                            <NavDropdown.Item href="event/stuff">Stuff</NavDropdown.Item>
+                            <NavDropdown.Item href="event/stuff">Stuff</NavDropdown.Item>
+                            <NavDropdown.Item href="event/stuff">Stuff</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Other">
+                            <NavDropdown.Item href="other/stuff">Stuff</NavDropdown.Item>
+                            <NavDropdown.Item href="other/stuff">Stuff</NavDropdown.Item>
+                            <NavDropdown.Item href="other/stuff">Stuff</NavDropdown.Item>
+                        </NavDropdown>
+                </Nav>
+                </Container>
+            </Container>
+            <Form className="d-flex">
+                <FormControl
+                    type="search"
+                    placeholder= ""
+                    className="mr-2"
+                    aria-label="Search"
+                />
+            </Form>
+            <Button> Book a Visit </Button>
+        </Navbar>
+      </div>  
     );
 
 };
