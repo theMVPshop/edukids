456561ad9f60f706861545173004125364389ad6
BenABaron <benabaron@gmail.com>
2021-09-28 18:32:25 -0500

    fixed styling organization

--- edukids/src/App.js
+++ edukids/src/App.js
@@ -1,5 +1,6 @@
 import Router from "./Router"
 import {BrowserRouter} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
