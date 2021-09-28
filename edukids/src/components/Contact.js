9c8266a841a5002dbf1923db890a4e71ea74085f
tcannetti <tcannetti@gmail.com>
2021-09-27 19:59:27 -0500

    icons and badges added to contact page

--- edukids/src/components/Contact.js
+++ edukids/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, Col, Container, Form, Row } from 'react-bootstrap';
+import { Badge, Card, Col, Container, Form, Row } from 'react-bootstrap';
+import { BsMap, BsPhone, BsFillEnvelopeOpenFill } from 'react-icons/bs'
 
 function Classes(props) {
   return(
@@ -7,29 +8,41 @@ function Classes(props) {
       <Row>
         <Col>
           <Row>
-            <Card></Card>
+            <Card>
+              <h1>Contact Us for Further Information</h1>
+              <h5>lorem ipsum blah blah blah</h5>
+            </Card>
           </Row>
           <Row>
-            <Card></Card>
+            <Card>
+              <BsMap />
+              <Badge bg="secondary">Location Address</Badge>
+            </Card>
           </Row>
           <Row>
-            <Card></Card>
+            <Card>
+              <BsFillEnvelopeOpenFill />
+              <Badge bg="secondary">Email Address</Badge>
+            </Card>
           </Row>
           <Row>
-            <Card></Card>
+            <Card>
+              <BsPhone />
+              <Badge bg="secondary">Phone Number</Badge>
+            </Card>
           </Row>
         </Col>
         <Col>
           <Card>
             <Form>
             <Form.Group classname="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Control as="textarea" placeholder="Name" />
+                <Form.Control type="text" placeholder="Name" />
               </Form.Group>
               <Form.Group classname="mb-3" controlId="exampleForm.ControlInput2">
                 <Form.Control type="email" placeholder="Email" />
               </Form.Group>
               <Form.Group classname="mb-3" controlId="exampleForm.ControlTextarea2">
-                <Form.Control as="textarea" placeholder="Subject" />
+                <Form.Control type="text" placeholder="Subject" />
               </Form.Group>
               <Form.Group classname="mb-3" controlId="exampleForm.ControlTextarea3">
                 <Form.Control as="textarea" placeholder="Message" rows={3} />
