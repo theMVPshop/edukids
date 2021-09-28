785f2f638c4996da705efb2bd71086ba53dd7473
Lance Hammond Jr <lance.hammondjr@gmail.com>
2021-09-27 20:06:29 -0500

    Lance's Updates

--- edukids/src/App.js
+++ edukids/src/App.js
@@ -2,12 +2,14 @@ import Router from "./Router"
 import {BrowserRouter} from 'react-router-dom'
 import './App.css';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
     <Header/>
     <Router/>
+    <Footer/>
     </BrowserRouter>
 
   );
