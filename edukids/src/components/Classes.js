37eae37dcab8e1ab6f12cf023687f177388032b0
eyokoo <70350730+eyokoo@users.noreply.github.com>
2021-10-04 19:55:15 -0500

    close to done with classes component styling

--- edukids/src/components/Classes.js
+++ edukids/src/components/Classes.js
@@ -9,7 +9,103 @@ function Classes(props) {
   return (
     <div>
       <Container>
-        <Row>
+        <Row className="Classes-Row-1">
+          <Col className="Classes-Col">
+            <Card className="Classes-Card" style={{backgroundColor:"rgb(254,93,55)"}} >
+              <Card.Img className="Classes-Card-Img"
+              variant="top" src="https://images.unsplash.com/photo-1505672678657-cc7037095e60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" />
+              <Card.Body className="Classes-Card-Body">
+                <Card.Title>Physical Class</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+                <Row >
+                  <Col className="Classes-Card-Col">
+                    <Card.Text>
+                      Age 2-5 Years
+                </Card.Text>
+                  </Col>
+                  <Col className="Classes-Card-Col">
+                    <Card.Text>
+                      Size 18 Seats
+                </Card.Text>
+                  </Col>
+                  <Col>
+                    <Card.Text>
+                      Price $22/m
+                </Card.Text>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col className="Classes-Col">
+            <Card className="Classes-Card" style={{backgroundColor:"rgb(234,170,20)" }}>
+              <Card.Img className="Classes-Card-Img" variant="top" src="https://images.unsplash.com/photo-1505672678657-cc7037095e60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" />
+              <Card.Body className="Classes-Card-Body">
+                <Card.Title>Creative Class</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+                <Row>
+                  <Col className="Classes-Card-Col">
+                    <Card.Text>
+                      Age
+                      2-5 Years
+                </Card.Text>
+                  </Col>
+                  <Col className="Classes-Card-Col">
+                    <Card.Text>
+                      Size
+                      18 Seats
+                </Card.Text>
+                  </Col>
+                  <Col>
+                    <Card.Text>
+                      Price
+                      $22/m
+                </Card.Text>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col className="Classes-Col">
+            <Card className="Classes-Card" style={{backgroundColor:"rgb(4,185,146)"}}>
+              <Card.Img className="Classes-Card-Img" variant="top" src="https://images.unsplash.com/photo-1505672678657-cc7037095e60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" />
+              <Card.Body className="Classes-Card-Body">
+                <Card.Title>Learning Class</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+                <Row>
+                  <Col className="Classes-Card-Col">
+                    <Card.Text>
+                      Age
+                      2-5 Years
+                </Card.Text>
+                  </Col>
+                  <Col className="Classes-Card-Col">
+                    <Card.Text>
+                      Size
+                      18 Seats
+                </Card.Text>
+                  </Col>
+                  <Col>
+                    <Card.Text>
+                      Price
+                      $22/m
+                </Card.Text>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+       
+        <Row className="Classes-Row-2">
           <Col className="Classes-Col">
             <Card className="Classes-Card" style={{backgroundColor:"rgb(254,93,55)"}} >
               <Card.Img className="Classes-Card-Img"
@@ -105,7 +201,7 @@ function Classes(props) {
           </Col>
         </Row>
 
-        <Row>
+        <Row className="Classes-Row-3">
           <Col>
             <Card.Body>
               <Card.Title>Get Online Class For Free</Card.Title>
@@ -114,8 +210,8 @@ function Classes(props) {
                 </Card.Text>
             </Card.Body>
           </Col>
-          <Col>
-            <Form>
+          <Col className="Classes-Col-Form">
+            <Form className="Classes-Form">
               <h4> Register Now</h4>
               <h8>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h8>
               <Form.Group className="mb-3" controlId="formBasicName">
@@ -125,9 +221,9 @@ function Classes(props) {
                 <Form.Control type="email" placeholder="Enter email" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicMsg" >
-                <Form.Control type="msg" placeholder="Message"/>
+                <Form.Control as="textarea" rows={4}type="msg"placeholder="Message"/>
               </Form.Group>
-              <Button variant="primary" type="submit">
+              <Button className="Classes-Form-Button" variant="primary" type="submit">
                 Submit Now
               </Button>
             </Form>
