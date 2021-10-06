d1c4219b460af6aa71a5266253e108dcab5bbea6
tonezone108 <tonezone108@gmail.com>
2021-10-05 20:02:54 -0500

    adding to team component

--- edukids/src/components/Team.js
+++ edukids/src/components/Team.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react'
 // import { Button } from "@material-ui/core";
 import { Col, Container, Row, Card, CardGroup } from 'react-bootstrap';
+import Image1 from '../img/portrait-of-smiling-male-school-teacher-standing-i-7BJLS3R.jpg';
+import Image2 from '../img/young-teacher-at-university-XG8ZTHV.jpg';
+import Image3 from '../img/education-teachers-university-and-schools-concept--JA74V7F.jpg'
+import Image4 from '../img/smiling-science-teacher-YXXHLVC.jpg'
+import Image5 from '../img/cheerful-women-in-printing-office-JQ5K4XE.jpg'
+
 
 class Team extends Component {
 
@@ -8,24 +14,27 @@ class Team extends Component {
     render() {
         return  (
             <Container >
+                                <Container className="Head-Container">
+      <h1 className="Head-H1">Team</h1>
+    </Container>
 <Row className="justify-content-md-center" >
 <CardGroup >
     
   <Card align="center" >
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
+    <Card.Img className="Team-Card-Image" src={Image1}/>
+    <Card.Body className="Team-Card-Body1">
       <Card.Title>John Doe</Card.Title>
       <Card.Text>
       Teacher
       </Card.Text>
       <Card.Text>Facebook Twitter Instagram Pinterest</Card.Text>
-    </Card.Body>
+    </Card.Body >
     <Card.Footer>
     </Card.Footer>
   </Card>
   <Card align="center">
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
+    <Card.Img className="Team-Card-Image" src={Image2}/>
+    <Card.Body className="Team-Card-Body2">
       <Card.Title>Sally Watson</Card.Title>
       <Card.Text>
       Teacher
@@ -36,8 +45,8 @@ class Team extends Component {
     </Card.Footer>
   </Card>
   <Card align="center">
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
+    <Card.Img className="Team-Card-Image" src={Image3}/>
+    <Card.Body className="Team-Card-Body3">
       <Card.Title>Mary James</Card.Title>
       <Card.Text>
       Teacher
@@ -52,8 +61,8 @@ class Team extends Component {
 <Row className="justify-content-md-center">
 <CardGroup>
 <Card align="center">
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
+    <Card.Img className="Team-Card-Image" src={Image4}/>
+    <Card.Body className="Team-Card-Body3">
       <Card.Title>Jimmer Housten</Card.Title>
       <Card.Text>
       Teacher
@@ -64,8 +73,8 @@ class Team extends Component {
     </Card.Footer>
   </Card>
   <Card align="center">
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
+    <Card.Img className="Team-Card-Image" src={Image5}/>
+    <Card.Body className="Team-Card-Body1">
       <Card.Title>Alison WHite</Card.Title>
       <Card.Text>
       Teacher
@@ -76,8 +85,8 @@ class Team extends Component {
     </Card.Footer>
   </Card>
   <Card align="center">
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
+    <Card.Img className="Team-Card-Image" src={Image5}/>
+    <Card.Body className="Team-Card-Body2">
       <Card.Title>Ana Scott</Card.Title>
       <Card.Text>
       Teacher
