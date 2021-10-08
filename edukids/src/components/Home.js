5edc8c0c12c6cdef95bdd2ccbcdc6e4e8d9dc7d3
eyokoo <70350730+eyokoo@users.noreply.github.com>
2021-10-07 19:49:47 -0500

    Half of Home Page CSS

--- edukids/src/components/Home.js
+++ edukids/src/components/Home.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react'
 import { Button, Card, Container, Col, ListGroup, Row } from 'react-bootstrap';
-import { FaFacebook , FaCheckCircle} from 'react-icons/fa'
+import { FaFacebook, FaCheckCircle } from 'react-icons/fa'
 class Home extends Component {
 
     render() {
         return (
             <div>
                 <Container>
-                    <Container className="Classes-Head-Container">
+                    <Container className="Home-Head-Container">
                         <Row>
                             <h1 className="Head-H1"> Let the Children Change the World Today!</h1>
                         </Row>
                         <Row>
                             <Col>
-                                <Button variant="primary" type="button">
+                                <Button className="Home-Button-a" variant="primary" type="button">
                                     Learn More
                        </Button>
 
-                                <Button variant="primary" type="button">
+                                <Button className="Home-Button-b" variant="primary" type="button">
                                     Contact Us
                        </Button>
                             </Col>
                         </Row>
                     </Container>
                     <Row>
-                        <h3>Discover School Activities</h3>
+                        <Card className="Home-Title-Body-a">
+                            <Card.Body>
+                                <Card.Title className="Home-Title">Discover School Activities</Card.Title>
+                            </Card.Body>
+                        </Card>
                     </Row>
                     <Row>
                         <Col className="Home-Card-Col">
-                            <Card style={{ backgroundColor: "rgb(4,185,146)" }}>
+                            <Card className="Home-Cards-a">
                                 <Card.Body>
                                     <Card.Title>Creative Games</Card.Title>
                                     <Card.Text>
@@ -38,7 +42,7 @@ class Home extends Component {
                             </Card>
                         </Col>
                         <Col className="Home-Card-Col">
-                            <Card style={{ backgroundColor: "rgb(254,93,55)" }}>
+                            <Card className="Home-Cards-b">
                                 <Card.Body>
                                     <Card.Title>Creative Learning</Card.Title>
                                     <Card.Text>
@@ -48,9 +52,11 @@ class Home extends Component {
                             </Card>
                         </Col>
                         <Col className="Home-Card-Col">
-                            <Card style={{ backgroundColor: "rgb(234,170,20)" }}>
+                            <Card className="Home-Cards-c">
                                 <Card.Body>
-                                    <Card.Title>Creative Activities</Card.Title>
+                                    <Card.Title>
+                                        Creative Activities
+                                        </Card.Title>
                                     <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of the card's content.
                                     </Card.Text>
@@ -64,27 +70,34 @@ class Home extends Component {
                             **Two Overlapping Image Goes Here...Below**
                         </Col>
                         <Col>
-                            <h4>Friendly School having proud of their Students</h4>
-
-                            <h8>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet porttitor eget dolor morbi non. At imperdiet dui accumsan sit amet nulla facilisi.</h8>
-
+                            <Card className="Home-Card">
+                                <Card.Body>
+                                    <Card.Title className="Home-Title">
+                                    Friendly School having proud of their Students
+                                    </Card.Title>
+                                    <Card.Text>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet porttitor eget dolor morbi non. At imperdiet dui accumsan sit amet nulla facilisi.
+                                    </Card.Text>
+                                </Card.Body>
                             <ListGroup>
-                                <ListGroup.Item className="Classes-List-Group-Item">
-                                    <span style={{ margin: "10px", color: "orange" }}><FaCheckCircle /></span>Lorem Ipsum
+                                <ListGroup.Item className="Home-List-Group-Item">
+                                    <span style={{ margin: "10px", color: "#fe5d37" }}><FaCheckCircle /></span>Lorem Ipsum
                                 </ListGroup.Item >
-                                <ListGroup.Item className="Classes-List-Group-Item">
-                                    <span style={{ margin: "10px", color: "orange" }}><FaCheckCircle /></span>Lorem Ipsum
+                                <ListGroup.Item className="Home-List-Group-Item">
+                                    <span style={{ margin: "10px", color: "#fe5d37" }}><FaCheckCircle /></span>Lorem Ipsum
                                 </ListGroup.Item>
-                                <ListGroup.Item className="Classes-List-Group-Item">
-                                    <span style={{ margin: "10px", color: "orange" }}><FaCheckCircle /></span>Lorem Ipsum
+                                <ListGroup.Item className="Home-List-Group-Item">
+                                    <span style={{ margin: "10px", color: "#fe5d37" }}><FaCheckCircle /></span>Lorem Ipsum
                                 </ListGroup.Item>
-                                <ListGroup.Item className="Classes-List-Group-Item">
-                                    <span style={{ margin: "10px", color: "orange" }}><FaCheckCircle /></span>Lorem Ipsum
+                                <ListGroup.Item className="Home-List-Group-Item">
+                                    <span style={{ margin: "10px", color: "#fe5d37" }}><FaCheckCircle /></span>Lorem Ipsum
                              </ListGroup.Item>
                             </ListGroup>
-                            <Button variant="primary" type="button">
+                            <Button className="Home-Button-a" variant="primary" type="button">
                                 More About Us
                             </Button>
+                            </Card>
+
                         </Col>
                     </Row>
 
@@ -93,7 +106,7 @@ class Home extends Component {
                     </Row>
                     {/**Don't know how to build this out...will keep working on it */}
 
-                    <Container className="Home-Head-Container">
+                    <Container className="Home-Bottom-Container">
                         <Row>
                             <h1 className="Head-H1">Choose Us For Your Child Better Future</h1>
                             <h8>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h8>
@@ -105,7 +118,7 @@ class Home extends Component {
                        </Button>
                             </Col>
                         </Row>
-                    </Container> 
+                    </Container>
 
 
                 </Container>
