95874abbdb8b14c996f0dfc45f8c203a66a25a9c
Lance Hammond Jr <lance.hammondjr@gmail.com>
2021-10-11 20:03:45 -0500

    Lance's Update

---
+++ edukids/src/components/GalImages.js
@@ -0,0 +1,34 @@
+
+import Image1 from '../img/adorable-kids-playing-with-constructor-on-floor-in-ZP6VC78.jpg';
+import Image2 from '../img/boys-and-girls-dancing-PZNK4D9.jpg';
+import Image3 from '../img/cheerful-women-in-printing-office-JQ5K4XE.jpg';
+import Image4 from '../img/cute-girl-playing-with-paint-in-art-class-85QTUD9.jpg';
+import Image5 from '../img/education-teachers-university-and-schools-concept--JA74V7F.jpg';
+import Image6 from '../img/emotion-emoticons-used-by-a-psychologist-during-a--D2BFM6T.jpg';
+import Image7 from '../img/happy-educator-hugging-adorable-kids-in-kindergart-CSSFQDV.jpg';
+import Image8 from '../img/happy-kids-are-painting-at-home-FSC2Q9C.jpg';
+import Image9 from '../img/kindergarten-age-girl-learning-about-the-planets-a-EKX3YDA.jpg';
+import Image10 from '../img/mother-or-nursery-teacher-teaches-her-children-to--QK27JT7.jpg';
+import Image11 from '../img/painting-together-P9VHLJH.jpg';
+import Image12 from '../img/sitting-on-a-carpet-P8BAWTX.jpg';
+
+
+const GalImages = [
+    { id: 1, src: {Image1}, title: 'indoor', description: 'indoor' },
+    { id: 2, src: {Image2}, title: 'outdoor', description: 'outdoor' },
+    { id: 3, src: {Image3}, title: 'painting', description: 'painting' },
+    { id: 4, src: {Image4}, title: 'painting', description: 'painting' },
+    { id: 5, src: {Image5}, title: 'outdoor', description: 'outdoor' },
+    { id: 6, src: {Image6}, title: 'indoor', description: 'indoor' },
+    { id: 7, src: {Image7}, title: 'indoor', description: 'indoor' },
+    { id: 8, src: {Image8}, title: 'painting', description: 'painting' },
+    { id: 9, src: {Image9}, title: 'indoor', description: 'indoor' },
+    { id: 10, src: {Image10}, title: 'indoor', description: 'indoor' },
+    { id: 11, src: {Image11}, title: 'painting', description: 'painting' },
+    { id: 12, src: {Image12}, title: 'indoor', description: 'indoor' },
+    
+]
+
+
+
+export default GalImages;
